5bb46d8e1448fff0bfde518b6b07edc2b9394c4d
SukminYun <yunsukmin522@gmail.com>
2024-09-24 21:22:36 +0900

    logic edit

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import Game from './Game.tsx';
 
 function App() {
   return (
-    <>
-      <div className="title">2048 Game</div>
-      <a href="https://play2048.co">Original game link</a>
+    <div className="game">
+      <div className="header">
+        <div className="title">2048 Game</div>
+        <a href="https://play2048.co">
+          <img
+            src="./assets/2048.png"
+            className="origin-link"
+            alt="Original game link"
+          />
+        </a>
+      </div>
       <div>
         <Game />
       </div>
-    </>
+    </div>
   );
 }
 
